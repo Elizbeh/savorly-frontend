6c5d7c109d6d603b8901e4cf628262715126626e
Elizbeh <eliz01_01@yahoo.com>
2025-05-13 23:20:10 +0200

    Trigger rebuild

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Set basename to match your GitHub Pages path
 const basename = import.meta.env.MODE === 'production' ? '/savorly-frontend' : '/';
 
 root.render(
   <React.StrictMode>
-      <Router basename={basename}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </Router>
+    <Router basename={basename}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );
